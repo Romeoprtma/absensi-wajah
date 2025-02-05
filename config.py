2482b451dc42c4ab7ec22675135d2c889887c3b6
unknown <romeomuliap@gmail.com>
2025-02-05 12:38:36 +0700

    update config db

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DB_CONFIG = {
-    "host": "romeoprtma.mysql.pythonanywhere-services.com",
-    "user": "romeoprtma",
-    "password": "huZ6i2rb@wEkaQr",
-    "database": "romeoprtma$absensi",
+    "host": "enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "user": "x5iencdujzp36twn",
+    "password": "we08y13y6kzbl3kd",
+    "database": "dduymovhobn8ag01",
+    "port": "3306"
 }

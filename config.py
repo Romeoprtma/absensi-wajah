f1e6ea6f4da89e4ecffd6675e7125b248f3daab4
unknown <romeomuliap@gmail.com>
2025-02-05 12:33:07 +0700

    update config db

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 DB_CONFIG = {
     "host": "romeoprtma.mysql.pythonanywhere-services.com",
     "user": "romeoprtma",
-    "password": "",
+    "password": "huZ6i2rb@wEkaQr",
     "database": "romeoprtma$absensi",
-    'port' : 3306
 }
